8ddb40906ef8f85e3727d8bfd444355ac2c39fd7
xiuyi <1216994857@qq.com>
2025-02-04 16:39:00 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+void sort(int array[],int size)
+{
+	
+	
+}
+
+int main()
+{
+	int array[] = {12,4,95,56,25,1};
+	int size = sizeof(array)/sizeof(array[0]);
+	sort(array,size);
+	return 0;
+}
